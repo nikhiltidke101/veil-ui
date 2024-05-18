02ea86772dc35e79c801e5a8e460486850b3d431
Nikhil Tidke <i-nikhil.tidke@devrev.ai>
2024-05-18 13:09:49 +0530

    first commit

---
+++ packages/ui/src/button.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return <Comp className={className} ref={ref} {...props} />;
+  }
+);
+Button.displayName = "Button";
+
+export { Button };
