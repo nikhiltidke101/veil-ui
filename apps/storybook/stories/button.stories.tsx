cd87c40109b98283325e4bc5b96129e1fcf25aa4
Nikhil Tidke <i-nikhil.tidke@devrev.ai>
2024-05-22 18:42:32 +0530

    fix: build

--- apps/storybook/stories/button.stories.tsx
+++ apps/storybook/stories/button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Button } from "@veil/ui/button";
+import { Button } from "ui/button";
 
 const meta: Meta<typeof Button> = {
   component: Button,
