7ba6ef86925573778c134fb255fb1c34618d2a15
Nikhil Tidke <i-nikhil.tidke@devrev.ai>
2024-08-01 18:50:02 +0530

    add: changesets

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Button } from "@repo/ui/button";
+import { Button as ButtonUI } from "@repo/ui-shared/button";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -49,6 +50,9 @@ export default function Home() {
         <Button appName="web" className={styles.secondary}>
           Open alert
         </Button>
+        <ButtonUI appName="web" className={styles.secondary}>
+          Open alert 2
+        </ButtonUI>
       </main>
       <footer className={styles.footer}>
         <a
---
+++ packages/ui-shared/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @repo/ui-shared
+
+## 1.0.0
+
+### Major Changes
+
+- button component added
